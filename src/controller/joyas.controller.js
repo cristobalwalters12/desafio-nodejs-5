06fe35809badc25a94469d1c07b99fe045cf5c81
Ayxa Chaverra <achaverrar@gmail.com>
2024-02-09 15:23:10 -0500

    use error handler in the getJoyas controller

--- src/controller/joyas.controller.js
+++ src/controller/joyas.controller.js
@@ -1,11 +1,11 @@
 const joyasModel = require("../models/joyas.model");
 
-const getJoyas = async (req, res) => {
+const getJoyas = async (req, res, next) => {
   try {
     const results = await joyasModel.getJoyasFromDB(req.query);
     res.status(200).json(results);
   } catch (error) {
-    res.status(500).json(error);
+    next(error);
   }
 };
 
