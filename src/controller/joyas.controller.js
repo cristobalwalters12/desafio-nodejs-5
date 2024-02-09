146fd57d632b0d7a110997819223155e8a389964
Ayxa Chaverra <achaverrar@gmail.com>
2024-02-09 16:50:16 -0500

    add an error catcher helper to simplify and wrap the controllers

--- src/controller/joyas.controller.js
+++ src/controller/joyas.controller.js
@@ -1,21 +1,13 @@
 const joyasModel = require("../models/joyas.model");
 
-const getJoyas = async (req, res, next) => {
-  try {
-    const results = await joyasModel.getByPageAndLimit(req.query);
-    res.status(200).json(results);
-  } catch (error) {
-    next(error);
-  }
+const getJoyas = async (req, res) => {
+  const results = await joyasModel.getByPageAndLimit(req.query);
+  res.status(200).json(results);
 };
 
 const getFilteredJoyas = async (req, res) => {
-  try {
-    const results = await joyasModel.getByFilters(req.query);
-    res.status(200).json(results);
-  } catch (error) {
-    res.status(500).json(error);
-  }
+  const results = await joyasModel.getByFilters(req.query);
+  res.status(200).json(results);
 };
 
 module.exports = { getJoyas, getFilteredJoyas };
--- src/routes/joyas.router.js
+++ src/routes/joyas.router.js
@@ -1,9 +1,16 @@
 const { Router } = require("express");
 const joyasController = require("../controller/joyas.controller");
 const { pageValidator, filtersValidator } = require("../DTO/joyas.dto");
+const errorCatcher = require("../helpers/errorCatcher");
 
 const router = Router();
 
-router.get("/", pageValidator, joyasController.getJoyas);
-router.get("/filtros", filtersValidator, joyasController.getFilteredJoyas);
+router.get("/", pageValidator, errorCatcher(joyasController.getJoyas));
+
+router.get(
+  "/filtros",
+  filtersValidator,
+  errorCatcher(joyasController.getFilteredJoyas)
+);
+
 module.exports = router;
