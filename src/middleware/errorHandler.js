658f37da3ca794009a7baca5b2f3b9ac4d2db903
Ayxa Chaverra <achaverrar@gmail.com>
2024-02-09 17:04:36 -0500

    refactor error handler to handle and display messages depending on the case and environment

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,15 @@
 const errorHandler = (err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: "Error interno del servidor" });
+  if (process.env.NODE_ENV !== "production") {
+    console.error(err.stack);
+  }
+  const { statusCode } = err;
+
+  if (statusCode) {
+    res.status(statusCode).json({ status: "fail", data: err.message });
+    return;
+  }
+
+  res.status(500).json({ status: "error", data: "Error interno del servidor" });
 };
 
 module.exports = errorHandler;
